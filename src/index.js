4ca527063c4b0d0c17a038f7005ae9973a153919
Huriosity <vetani3123@mail.ru>
2020-10-06 10:43:48 +0300

    feat: complete morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,26 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let words = expr.split("**********");
+    let result = "";
+    let letters = [];
+    for (let i = 0; i < words.length; i += 1) {
+        for (let j = 0; j < words[i].length / 10; j += 1) {
+            letters[i] = words[i].substr(j * 10, 10);
+            if (letters[i].length !== 10) {
+                while (letters[i].length !== 10) {
+                    letters[i] = "0" + letters[i];
+                }
+            }
+            letters[i] = letters[i].replace(/10/g,".").replace(/11/g,"-");
+            letters[i] = letters[i].replace(/0/g,"");
+            result +=  MORSE_TABLE[letters[i]]
+      }
+      if (i !== words.length -1) {
+          result += " ";
+      }
+    }
+    return result;
 }
 
 module.exports = {
